7984d5c3b9190e150246b300574ddbd62583bd88
gregleew567 <gregleew567@JVAN-312-2.hcdsb.edu.on.ca>
2022-02-04 09:34:18 -0500

    New Branch

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programmer: Gregory Lee Wo
+ * Date: 04 Feburary 2022
+ * Student ID: 991 638 141
+ * Project: CardCode
+ * Create a new CardDealer class
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
@@ -9,12 +11,12 @@ package ca.sheridancollege.week2.softwarefundamentals;
  * The class where we create Cards
  * @author Gregory Lee Wo, 04 Feb 2022s
  */
-public class CardDealer 
-{
-    public static void main(String[] args)
-    {
-        Card nineClubs = new Card("Clbs", 9);
+public class CardDealer {
+    
+    public static void main(String[] args) {
+        Card nineClubs = new Card("Clubs", 9);
         nineClubs.setSuit("diamonds");
+        Card aceHearts = new Card("Hearts", 1);
     }
+    
 }
-// New Branch
